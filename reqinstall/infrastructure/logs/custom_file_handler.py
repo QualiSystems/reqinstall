a05c5c99f4d8b54edaa934939101e2a8cf2ce6f8
Johnathan Viduchinsky <johnathan.v@qualisystems.com>
2019-01-09 12:30:00 +0200

    fixing a bug that failed the create log hierarchy when it isn't exists

--- reqinstall/infrastructure/logs/custom_file_handler.py
+++ reqinstall/infrastructure/logs/custom_file_handler.py
@@ -7,8 +7,8 @@ class CreateHierarchyFileHandler(logging.FileHandler):
     """ Custom file handler that creates the path of the log file if it's not exists """
 
     def __init__(self, filename, mode='a', encoding=None, delay=0):
-        super(CreateHierarchyFileHandler, self).__init__(filename, mode, encoding, delay)
         self._create_path_hierarchy(os.path.dirname(filename))
+        super(CreateHierarchyFileHandler, self).__init__(filename, mode, encoding, delay)
 
     @staticmethod
     def _create_path_hierarchy(path):
