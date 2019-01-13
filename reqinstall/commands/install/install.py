c4e0a6afa3df6cb87daab589365b6a7523a8dd20
Johnathan Viduchinsky <johnathan.v@qualisystems.com>
2019-01-13 16:00:19 +0200

    fixing --extra-index-url

--- reqinstall/commands/install/install.py
+++ reqinstall/commands/install/install.py
@@ -11,26 +11,23 @@ class PipInstallCommand(object):
         logger.info("Running {}".format(' '.join(arguments)))
         return subprocess.call(arguments)
 
-    def _install_arguments(self, namespace):
+    @staticmethod
+    def _install_arguments(namespace):
         arguments = []
         if namespace.packages:
-            self._add_argument(arguments, namespace.packages)
+            arguments += namespace.packages
         if namespace.req_path:
-            self._add_argument(arguments, ['-r', namespace.req_path])
+            arguments += ['-r', namespace.req_path]
         if namespace.local_path:
-            self._add_argument(arguments, ['--no-index',
-                                           '--find-links', '{0}'.format(namespace.local_path)])
+            arguments += ['--no-index', '--find-links', '{0}'.format(namespace.local_path)]
         if namespace.index_url:
-            self._add_argument(arguments, ['--index-url', '{0}'.format(namespace.index_url)])
+            arguments += ['--index-url', '{0}'.format(namespace.index_url)]
         if namespace.extra_index_url:
-            self._add_argument(arguments, ['--extra-index-url', '{0}'.format(namespace.extra_index_url)])
+            for index_url in namespace.extra_index_url:
+                arguments += ['--extra-index-url', index_url]
         if namespace.trusted_host:
-            self._add_argument(arguments, ['--trusted-host', namespace.trusted_host])
+            arguments += ['--trusted-host', namespace.trusted_host]
         if namespace.log:
-            self._add_argument(arguments, ['--log', namespace.log])
-        self._add_argument(arguments, ['--upgrade', '--no-input', '--disable-pip-version-check', '--verbose'])
+            arguments += ['--log', namespace.log]
+        arguments += ['--upgrade', '--no-input', '--disable-pip-version-check', '--verbose']
         return arguments
-
-    @staticmethod
-    def _add_argument(arguments_list, value):
-        arguments_list += value
